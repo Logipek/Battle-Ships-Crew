4ee0f8ff33de3069da87a99d7147bc9627e038b9
JMouzet <julien.mouzet@coda-student.school>
2024-12-14 18:48:19 +0100

    Refactor player grid logic to use boatSize variable and improve event handling

--- test_script.js
+++ test_script.js
@@ -3,7 +3,7 @@
 class PlayerGrid {
     constructor() {
         this.grid = [];
-        this.boat = 2;
+        this.boatSize = 2;
         this.horizontal = true;
         this.initGrid();
         this.setEventsRadio();
@@ -21,63 +21,74 @@ class PlayerGrid {
 
     setEventsRadio() {
         document.querySelectorAll('input[type="radio"]').forEach(radio => {
-            radio.addEventListener('change', function() {
-                this.boat = this.value;
-                console.log('Boat size:', this.boat);
+            radio.addEventListener('change', e => {
+                this.boatSize = parseInt(e.target.value);
+                console.log('Boat size:', this.boatSize);
             });
         });
     }
 
     updateTable() {
-        document.querySelectorAll('table tr:not(:first-child) td:not(:first-child)').forEach(td => {
+        document.querySelectorAll('td.main').forEach(td => {
             let x = td.cellIndex - 1;
             let y = td.parentNode.rowIndex - 1;
             if (this.grid[y][x] === 0) {
-                td.style.backgroundColor = 'lightskyblue';
+                td.children[0].style.backgroundColor = 'lightskyblue';
             } else {
-                td.style.backgroundColor = 'gold';
+                td.children[0].style.backgroundColor = 'gold';
+            }
+        });
+    }
+
+    removeBoat() {
+        document.querySelectorAll('td.main').forEach(td => {
+            let x = td.cellIndex - 1;
+            let y = td.parentNode.rowIndex - 1;
+            if (this.grid[y][x] === this.boatSize) {
+                this.grid[y][x] = 0;
             }
         });
     }
     
     setEventsTable() {
-        document.querySelectorAll('table tr:not(:first-child) td:not(:first-child)').forEach(td => {
+        document.querySelectorAll('td.main').forEach(td => {
             td.addEventListener('contextmenu', e => {
                 e.preventDefault();
+                td.dispatchEvent(new MouseEvent('mouseout'));
                 this.horizontal = !this.horizontal;
+                td.dispatchEvent(new MouseEvent('mouseover'));
                 console.log('Horizontal:', this.horizontal);
             });
             td.addEventListener('mouseover', e => {
                 let x = e.target.cellIndex;
                 let y = e.target.parentNode.rowIndex;
                 if (this.horizontal) {
-                    if (x + this.boat > 11) {
-                        e.target.parentNode.children[x].style.backgroundColor = 'lightcoral';
+                    if (x + this.boatSize > 11) {
+                        //e.target.parentNode.children[x].children[0].style.backgroundColor = 'lightcoral';
                         return;
                     }
-                    for (let i = x; i < x + this.boat; i++) {
-                        if (e.target.parentNode.children[i].style.backgroundColor === 'gold') {
-                            e.target.parentNode.children[i].style.backgroundColor = 'lightcoral';
-                            return;
+                    for (let i = x; i < x + this.boatSize; i++) {
+                        if (e.target.parentNode.children[i].children[0].style.backgroundColor === 'gold') {
+                            e.target.parentNode.children[i].children[0].style.backgroundColor = 'lightcoral';
                         }
-                        e.target.parentNode.children[i].style.backgroundColor = 'lightgreen';
+                        e.target.parentNode.children[i].children[0].style.backgroundColor = 'lightgreen';
                     }
                 } else {
-                    if (y + this.boat > 11) {
+                    if (y + this.boatSize > 11) {
                         return;
                     }
-                    for (let i = y; i < y + this.boat; i++) {
-                        e.target.parentNode.parentNode.children[i].children[x].style.backgroundColor = 'lightgreen';
+                    for (let i = y; i < y + this.boatSize; i++) {
+                        e.target.parentNode.parentNode.children[i].children[x].children[0].style.backgroundColor = 'lightgreen';
                     }
                 }
             });
             td.addEventListener('mouseout', e => {
-                document.querySelectorAll('table tr:not(:first-child) td:not(:first-child)').forEach(td => {
+                document.querySelectorAll('td.main').forEach(td => {
                     this.updateTable();
-                    if (td.style.backgroundColor === 'gold') {
+                    if (td.children[0].style.backgroundColor === 'gold') {
                         return;
                     }
-                    td.style.backgroundColor = 'lightskyblue';
+                    td.children[0].style.backgroundColor = 'lightskyblue';
                 });
             });
             td.addEventListener('click', e => {
@@ -85,32 +96,32 @@ class PlayerGrid {
                 let y = e.target.parentNode.rowIndex - 1;
                 console.log('Clicked:', x, y);
                 if (this.horizontal) {
-                    if (x + this.boat > 10) {
+                    if (x + this.boatSize > 10) {
                         console.log('Out of bounds');
                         return;
                     }
-                    for (let i = x; i < x + this.boat; i++) {
+                    for (let i = x; i < x + this.boatSize; i++) {
                         if (this.grid[y][i] !== 0) {
-                            console.log('Boat overlap');
+                            this.removeBoat();
                             return;
                         }
                     }
-                    for (let i = x; i < x + this.boat; i++) {
-                        this.grid[y][i] = this.boat;
+                    for (let i = x; i < x + this.boatSize; i++) {
+                        this.grid[y][i] = this.boatSize;
                     }
                 } else {
-                    if (y + this.boat > 10) {
+                    if (y + this.boatSize > 10) {
                         console.log('Out of bounds');
                         return;
                     }
-                    for (let i = y; i < y + this.boat; i++) {
+                    for (let i = y; i < y + this.boatSize; i++) {
                         if (this.grid[i][x] !== 0) {
-                            console.log('Boat overlap');
+                            this.removeBoat();
                             return;
                         }
                     }
-                    for (let i = y; i < y + this.boat; i++) {
-                        this.grid[i][x] = this.boat;
+                    for (let i = y; i < y + this.boatSize; i++) {
+                        this.grid[i][x] = this.boatSize;
                     }
                 }
                 console.log('Grid:', this.grid);
