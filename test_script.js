59000434cd1e9b64eb6124aca7a38d8fafec679f
JMouzet <julien.mouzet@coda-student.school>
2024-12-16 09:57:58 +0100

    Refactor player grid logic to manage boat IDs and enhance event handling for boat placement. Added border radius to boats.

--- test_script.js
+++ test_script.js
@@ -4,6 +4,7 @@ class PlayerGrid {
     constructor() {
         this.grid = [];
         this.boatSize = 2;
+        this.boatId = 3;
         this.horizontal = true;
         this.initGrid();
         this.setEventsRadio();
@@ -28,6 +29,34 @@ class PlayerGrid {
         });
     }
 
+    resetRadius(elDiv) {
+        elDiv.style.borderTopLeftRadius = '0';
+        elDiv.style.borderTopRightRadius = '0';
+        elDiv.style.borderBottomLeftRadius = '0';
+        elDiv.style.borderBottomRightRadius = '0';
+    }
+
+    borderRadius(elDiv, start) {
+        this.resetRadius(elDiv);
+        if (this.horizontal) {
+            if (start) {
+                elDiv.style.borderTopLeftRadius = '50%';
+                elDiv.style.borderBottomLeftRadius = '50%';
+                return;
+            }
+            elDiv.style.borderTopRightRadius = '50%';
+            elDiv.style.borderBottomRightRadius = '50%';
+            return;
+        }
+        if (start) {
+            elDiv.style.borderTopLeftRadius = '50%';
+            elDiv.style.borderTopRightRadius = '50%';
+            return;
+        }
+        elDiv.style.borderBottomLeftRadius = '50%';
+        elDiv.style.borderBottomRightRadius = '50%';
+    }
+
     updateTable() {
         document.querySelectorAll('td.main').forEach(td => {
             let x = td.cellIndex - 1;
@@ -40,91 +69,164 @@ class PlayerGrid {
         });
     }
 
-    removeBoat() {
-        document.querySelectorAll('td.main').forEach(td => {
-            let x = td.cellIndex - 1;
-            let y = td.parentNode.rowIndex - 1;
-            if (this.grid[y][x] === this.boatSize) {
-                this.grid[y][x] = 0;
-            }
+    removeBoat(boatId) {
+        //Remove boat from grid and refactor grid
+        this.grid.forEach(row => {
+            row.forEach((cell, index) => {
+                if (cell === boatId) {
+                    row[index] = 0;
+                }
+                if (cell > boatId) {
+                    row[index]--;
+                }
+            });
         });
+        this.boatId--;
+        //Remove boat from table
+        this.updateTable();
     }
     
     setEventsTable() {
         document.querySelectorAll('td.main').forEach(td => {
+            //Contextmenu event
             td.addEventListener('contextmenu', e => {
+                //Prevent default context menu
                 e.preventDefault();
+                //Send mouseout event to remove green cells
                 td.dispatchEvent(new MouseEvent('mouseout'));
+                //Change direction
                 this.horizontal = !this.horizontal;
+                //Send mouseover event to redraw green cells
                 td.dispatchEvent(new MouseEvent('mouseover'));
-                console.log('Horizontal:', this.horizontal);
             });
+            //Mouseover event
             td.addEventListener('mouseover', e => {
                 let x = e.target.cellIndex;
                 let y = e.target.parentNode.rowIndex;
+                //For horizontal direction
                 if (this.horizontal) {
-                    if (x + this.boatSize > 11) {
-                        //e.target.parentNode.children[x].children[0].style.backgroundColor = 'lightcoral';
+                    //If the cell contains a boat, change color to red
+                    if (e.target.parentNode.children[x].children[0].style.backgroundColor === 'gold') {
+                        e.target.parentNode.children[x].children[0].style.backgroundColor = 'lightcoral';
                         return;
                     }
+                    //If the boat is out of bounds, return
+                    if (x + this.boatSize > 11) {
+                        return;
+                    }
+                    //Check if the boat overlaps with another boat
+                    let overlap = false;
                     for (let i = x; i < x + this.boatSize; i++) {
                         if (e.target.parentNode.children[i].children[0].style.backgroundColor === 'gold') {
-                            e.target.parentNode.children[i].children[0].style.backgroundColor = 'lightcoral';
+                            overlap = true;
+                            break;
+                        }
+                    }
+                    //If there's an overlap, return
+                    if (overlap) {
+                        return;
+                    }
+                    //If not, change color to green
+                    for (let i = x; i < x + this.boatSize; i++) {
+                        if (i === x) {
+                            this.borderRadius(e.target.parentNode.children[i].children[0], true);
+                        }
+                        else if (i === x + this.boatSize - 1) {
+                            this.borderRadius(e.target.parentNode.children[i].children[0], false);
+                        }
+                        else {
+                            this.resetRadius(e.target.parentNode.children[i].children[0]);
                         }
                         e.target.parentNode.children[i].children[0].style.backgroundColor = 'lightgreen';
                     }
+                //For vertical direction
                 } else {
+                    //If the cell contains a boat, change color to red
+                    if (e.target.parentNode.parentNode.children[y].children[x].children[0].style.backgroundColor === 'gold') {
+                        e.target.parentNode.parentNode.children[y].children[x].children[0].style.backgroundColor = 'lightcoral';
+                        return;
+                    }
+                    //If the boat is out of bounds, return
                     if (y + this.boatSize > 11) {
                         return;
                     }
+                    //Check if the boat overlaps with another boat
+                    let overlap = false;
                     for (let i = y; i < y + this.boatSize; i++) {
+                        if (e.target.parentNode.parentNode.children[i].children[x].children[0].style.backgroundColor === 'gold') {
+                            overlap = true;
+                            break;
+                        }
+                    }
+                    //If there's an overlap, return
+                    if (overlap) {
+                        return;
+                    }
+                    //If not, change color to green
+                    for (let i = y; i < y + this.boatSize; i++) {
+                        if (i === y) {
+                            this.borderRadius(e.target.parentNode.parentNode.children[i].children[x].children[0], true);
+                        }
+                        else if (i === y + this.boatSize - 1) {
+                            this.borderRadius(e.target.parentNode.parentNode.children[i].children[x].children[0], false);
+                        }
+                        else {
+                            this.resetRadius(e.target.parentNode.parentNode.children[i].children[x].children[0]);
+                        }
                         e.target.parentNode.parentNode.children[i].children[x].children[0].style.backgroundColor = 'lightgreen';
                     }
                 }
             });
+            //Mouseout event
             td.addEventListener('mouseout', e => {
-                document.querySelectorAll('td.main').forEach(td => {
-                    this.updateTable();
-                    if (td.children[0].style.backgroundColor === 'gold') {
-                        return;
-                    }
-                    td.children[0].style.backgroundColor = 'lightskyblue';
-                });
+                //Redraw the table
+                this.updateTable();
             });
+            //Click event
             td.addEventListener('click', e => {
                 let x = e.target.cellIndex - 1;
                 let y = e.target.parentNode.rowIndex - 1;
-                console.log('Clicked:', x, y);
+                //For horizontal direction
                 if (this.horizontal) {
+                    //Check if the boat overlaps with another boat
+                    for (let i = x; i < x + this.boatSize; i++) {
+                        if (this.grid[y][i] !== 0) {
+                            //If there's an overlap, remove the boat
+                            this.removeBoat(this.grid[y][i]);
+                            return;
+                        }
+                    }
+                    //If the boat is out of bounds, return
                     if (x + this.boatSize > 10) {
                         console.log('Out of bounds');
                         return;
                     }
+                    //If not, add the boat to the grid
                     for (let i = x; i < x + this.boatSize; i++) {
-                        if (this.grid[y][i] !== 0) {
-                            this.removeBoat();
+                        this.grid[y][i] = this.boatId;
+                    }
+                //For vertical direction
+                } else {
+                    //Check if the boat overlaps with another boat
+                    for (let i = y; i < y + this.boatSize; i++) {
+                        if (this.grid[i][x] !== 0) {
+                            //If there's an overlap, remove the boat
+                            this.removeBoat(this.grid[i][x]);
                             return;
                         }
                     }
-                    for (let i = x; i < x + this.boatSize; i++) {
-                        this.grid[y][i] = this.boatSize;
-                    }
-                } else {
+                    //If the boat is out of bounds, return
                     if (y + this.boatSize > 10) {
                         console.log('Out of bounds');
                         return;
                     }
+                    //If not, add the boat to the grid
                     for (let i = y; i < y + this.boatSize; i++) {
-                        if (this.grid[i][x] !== 0) {
-                            this.removeBoat();
-                            return;
-                        }
-                    }
-                    for (let i = y; i < y + this.boatSize; i++) {
-                        this.grid[i][x] = this.boatSize;
+                        this.grid[i][x] = this.boatId;
                     }
                 }
-                console.log('Grid:', this.grid);
+                //Increment boatId and redraw the table
+                this.boatId++;
                 this.updateTable();
             });
         });
